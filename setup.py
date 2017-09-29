d66e65055569a34987b00327eb5266d02a43f79f
Dan D'Avella <drdavella@gmail.com>
2017-09-29 12:59:35 -0400

    Add Alpha development status to classifiers

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
         'Framework :: Pytest',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
