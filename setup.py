168c6c579ebbe95901153713524213e1ec48f312
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-09-14 15:46:40 -0400

    MNT: More infrastructure update

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import sys
-import setuptools
-from distutils.version import LooseVersion
 from setuptools import setup
-
-# Setuptools 30.3.0 or later is needed for setup.cfg options to be used
-if LooseVersion(setuptools.__version__) < LooseVersion('30.3.0'):
-    sys.stderr.write("ERROR: sphinx-automodapi requires setuptools 30.3.0 or "
-                     "later (found {0})".format(setuptools.__version__))
-    sys.exit(1)
 
 setup(use_scm_version={'write_to': os.path.join('pytest_remotedata', 'version.py')})
